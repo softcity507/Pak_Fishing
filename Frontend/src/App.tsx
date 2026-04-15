6f791b5b601ccec8ac37ea8d7211697f7063c073
Sadaqat Ali <136867199+softcity507@users.noreply.github.com>
2026-04-15 15:44:06 +0500

    frontend is add Now

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='border flex flex-col h-screen justify-between'>
+      <header className='border'>
+        Header
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi?
+      </header>
+      <div className='border'>My Pak Fishing App</div>
+
+      <footer className='border'>
+        Footer
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sapiente!
+      </footer>
+    </div>
+  )
+}
+
+export default App
