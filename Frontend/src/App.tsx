302d88a03692dc7233b8166207cb402184444380
Sadaqat Ali <136867199+softcity507@users.noreply.github.com>
2026-05-02 07:10:04 +0500

    check all is ok in github action

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className='border flex flex-col h-screen justify-between'>
       <header className='border'>
         Header
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus, maiores!
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, commodi?
       </header>
       <div className='border'>My Pak Fishing App</div>
